81b004a91fba6f86033f87d8a60f4532c9b67348
Thomas Allen <thomasallenmartinho@gmail.com>
2024-05-29 12:48:55 +0200

    commit before starting

--- app/javascript/controllers/heart_controller.js
+++ app/javascript/controllers/heart_controller.js
@@ -1,32 +1,38 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="heart"
 export default class extends Controller {
-static targets = ["iconfill", "iconempty"]
+  static targets = ['iconfill', 'iconempty']
 
   connect() {
-    console.log("hellooo from heart controller");
+    // console.log("hellooo from heart controller");
   }
 
   favorite(event) {
     event.preventDefault()
-    console.log(this.iconemptyTarget.parentElement.href);
-    this.iconemptyTarget.parentElement.classList.add("hide-icon")
-    this.iconfillTarget.parentElement.classList.remove("hide-icon")
+    console.log(this.iconemptyTarget.parentElement.href)
+    this.iconemptyTarget.parentElement.classList.add('hide-icon')
+    this.iconfillTarget.parentElement.classList.remove('hide-icon')
     fetch(this.iconemptyTarget.parentElement.href, {
-      method: "POST",
-      headers: { "Accept": "application/json", "Content-Type": "application/json" }
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
     })
   }
 
   unfavorite(event) {
     event.preventDefault()
-    this.iconemptyTarget.parentElement.classList.remove("hide-icon")
-    this.iconfillTarget.parentElement.classList.add("hide-icon")
+    this.iconemptyTarget.parentElement.classList.remove('hide-icon')
+    this.iconfillTarget.parentElement.classList.add('hide-icon')
 
     fetch(this.iconfillTarget.parentElement.href, {
-      method: "DELETE",
-      headers: { "Accept": "application/json", "Content-Type": "application/json" }
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
     })
   }
 }
